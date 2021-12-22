82de8080d437ec404489a0d29d5e77205921570c
Honshu11 <egonza67@yahoo.com>
2021-12-21 18:55:51 -0800

    added heroku to twitter showcase app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container'>
+      <header>
+        <h1>Twitter Showcase</h1>
+      </header>
+      <body>
+        <h4>body of twitter app</h4>
+      </body>
+      <footer>
+        <h6>footer</h6>
+      </footer>
+    </div>
+  )
+}
+
+export default App;
