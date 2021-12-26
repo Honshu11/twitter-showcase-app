05567ca3528155c788e426dd92fb10d04c939975
Honshu11 <egonza67@yahoo.com>
2021-12-25 22:38:08 -0800

    added header, row, columns to twitter app. Need to center them.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col } from 'react-bootstrap-v5';
+
 
 function App() {
   return (
-    <div className='container'>
-      <header>
-        <h1>Twitter Showcase</h1>
+    <>
+    <div className='container-header'>
+      <header className='header mt-3'>
+        <h1>Twitter</h1>
       </header>
-      <body>
-        <h4>body of twitter app</h4>
-      </body>
-      <footer>
-        <h6>footer</h6>
-      </footer>
     </div>
+      <Container className="body-container">
+        <Row className="row">
+          <Col>Left Column</Col>
+          <Col xs={5}>Middle Column</Col>
+          <Col>Right Column</Col>
+        </Row>
+      </Container>
+    <footer className='footer mt-3'>
+      <h5>This is the footer</h5>
+    </footer>
+    </>
+    
   )
 }
+
+
 
 export default App;
