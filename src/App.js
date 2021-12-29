bc78fe846fc60b12561880c72376722ae6df0700
Honshu11 <egonza67@yahoo.com>
2021-12-28 21:36:04 -0800

    moved header and footer elements into components folder.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 import { Container, Row, Col } from 'react-bootstrap-v5';
 
 
 function App() {
   return (
     <>
-    <div className='container-header'>
-      <header className='header mt-3'>
-        <h1>Twitter</h1>
-      </header>
-    </div>
-      <Container className="body-container">
-        <Row className="row">
-          <Col>Left Column</Col>
-          <Col xs={5}>Middle Column</Col>
-          <Col>Right Column</Col>
-        </Row>
-      </Container>
-    <footer className='footer mt-3'>
-      <h5>This is the footer</h5>
-    </footer>
+      <Header />
+        <Container fluid="lg">
+          <Row className="row">
+            <Col>Left Column</Col>
+            <Col xs={5}>Middle Column</Col>
+            <Col>Right Column</Col>
+          </Row>
+        </Container>
+      <Footer />
     </>
     
   )
 }
-
-
 
 export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <div className="footer mt-3">
+            <footer>
+                <h6>This is the footer</h6>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
